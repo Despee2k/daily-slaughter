c8e229861b4623a7a583d3f57e4d698c3fc42d80
Dempsey <dagantareynat@gmail.com>
2024-12-31 18:14:56 +0800

    chore(loginpage): removed unused css

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -2,8 +2,8 @@ import LoginForm from "../Components/Login/LoginForm"
 
 const LoginPage = () => {
   return (
-    <div className="w-screen h-screen">
-      <h1 className="text-[#808180] font-extrabold text-3xl mb-16 p-4">Daily Slaughter</h1>
+    <div>
+      <h1 className="text-[#808180] font-extrabold text-2xl mb-16 p-4">Daily Slaughter</h1>
       <div className="flex justify-center mb-4">
         <h1 className="text-[#808180] font-bold text-3xl">LOGIN</h1>
       </div>
