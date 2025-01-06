81b48fa810e57b9893fa02d0afc2ebc4695afab1
Dempsey <dagantareynat@gmail.com>
2025-01-06 21:43:15 +0800

    feat(server): add basic routes and CRUD middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const db = require('./queries');
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
 app.get("/", (req, res) => {
-    res.send("HELLO WORLD!");
+    res.json({ info: "Node.js, Express, and Postgres API" });
 });
+
+app.get("/entries-by-date", db.getEntriesByDate);
+app.post("/add-entry", db.addEntry);
+app.put("/update-entry", db.updateEntry);
+app.delete("/delete-entry", db.deleteEntry);
 
 app.listen(PORT, () => console.log("Server is listening!"));
